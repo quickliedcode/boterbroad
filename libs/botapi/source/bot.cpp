64ed2fdb51fdf28a7babc1de61114566abeae6d0
Your Name <quickliedcode@yandex.ru>
2019-04-19 19:15:31 +0300

    code

--- libs/botapi/source/bot.cpp
+++ libs/botapi/source/bot.cpp
@@ -1,11 +1,15 @@
 #include <bot.h>
 
-TelegramBot::TelegramBot(string tok) : httpsSocket("api.telegram.org"){
-    string q = "GET https://api.telegram.org/bot";
-    q.append(tok);
-    q.append("/getUpdates HTTP/1.1\r\nHOST:api.telegram.org\r\nConnection: Keep-Alive\r\n\r\n\0");
-    HTTPResponse str = httpsSocket.query(q);
-    json result = str.toJSON();
+TelegramBot::TelegramBot(string tok) : httpsSocket("api.telegram.org"), token(tok){
+    json result = method("/getUpdates").toJSON();
     if(result["ok"] == false)
         throw runtime_error("Invalid bot token");
+}
+
+HTTPResponse TelegramBot::method(string method, string args = ""){
+    string q = "GET https://api.telegram.org/bot";
+    q.append(token);
+    q.append(method);
+    q.append(" HTTP/1.1\r\nHOST:api.telegram.org\r\nConnection: Keep-Alive\r\n\r\n");
+    return httpsSocket.query(q);
 }
