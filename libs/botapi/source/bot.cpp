8ced7399a5108f7a3b04c7e771d7d39d9131b63c
Your Name <quickliedcode@yandex.ru>
2019-04-19 23:26:22 +0300

    code

--- libs/botapi/source/bot.cpp
+++ libs/botapi/source/bot.cpp
@@ -10,6 +10,7 @@ HTTPResponse TelegramBot::method(const string& method, const string& args){
     string q = "GET https://api.telegram.org/bot";
     q.append(token);
     q.append(method);
+    q.append(args);
     q.append(" HTTP/1.1\r\nHOST:api.telegram.org\r\nConnection: Keep-Alive\r\n\r\n");
     return httpsSocket.query(q);
 }
