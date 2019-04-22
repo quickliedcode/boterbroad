28c0e0da6973fc6be609d74aa257d873bd8b3ae9
Your Name <quickliedcode@yandex.ru>
2019-04-22 17:03:41 +0300

    code

--- main.cpp
+++ main.cpp
@@ -7,45 +7,29 @@
 #define accept_message "Сообщение принято. Надеюсь, оно действительно смешное"
 
 int main(){
-    cout << "test\n";
     TelegramBot gbot(api_token);
     gbot.regAnswer("/start", start_message);
     while(1){
 
-        cout << endl << endl << " ------ here 1488 ------ " << endl << endl;
-
         json answer = gbot.getUpdates();
-
-        cout << endl << endl << " ------ here 2 ------ " << endl << endl;
 
         if(answer["result"].size())
             if(!gbot.sendKnownPhrase(answer)){
 
-                cout << endl << endl << " ------ here 3 ------ " << endl << endl;
+                gbot.sendMessage(answer["result"][0]["message"]["chat"]["id"], accept_message);
 
-                //gbot.sendMessage(answer["result"][0]["message"]["chat"]["id"], accept_message);
-
-                cout << endl << endl << " ------ here 4 ------ " << endl << endl;
-
-                try {
-                    int message_id = 0;
-                    int from_chat_id = 0;
-                    if(!answer["result"][0]["message"].is_null()){
-                        message_id = answer["result"][0]["message"]["message_id"];
-                        from_chat_id = answer["result"][0]["message"]["chat"]["id"];
-                    }else{
-                        message_id = answer["result"][0]["channel_post"]["message_id"];
-                        from_chat_id = answer["result"][0]["channel_post"]["chat"]["id"];
-                    }
-
-                    gbot.forwardMessage("@growbucket", from_chat_id, message_id);
-                } catch (...) {
-                    cout << "errorka";
+                int message_id = 0;
+                int from_chat_id = 0;
+                if(!answer["result"][0]["message"].is_null()){
+                    message_id = answer["result"][0]["message"]["message_id"];
+                    from_chat_id = answer["result"][0]["message"]["chat"]["id"];
+                }else{
+                    message_id = answer["result"][0]["channel_post"]["message_id"];
+                    from_chat_id = answer["result"][0]["channel_post"]["chat"]["id"];
                 }
 
-
+                gbot.forwardMessage("@growbucket", from_chat_id, message_id);
             }
-
         gbot.refresh();
     }
 }
